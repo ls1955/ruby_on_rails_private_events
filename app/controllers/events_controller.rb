daf02895b10de6cc9a679a4ce207b614167b7049
Cheong <xxlsxx1955@gmail.com>
2023-03-30 20:44:22 +0800

    Include events' routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,20 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :location)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: :show
-  resources :events, only: :index
+  resources :events, only: %i[index show new create]
 
   root "events#index"
 end
