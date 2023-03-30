8134fcd4bed4dcb7901b3781aaab9f19d5bd0f76
Cheong <xxlsxx1955@gmail.com>
2023-03-30 19:56:05 +0800

    Include UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
   resources :events, only: :index
 
   root "events#index"
