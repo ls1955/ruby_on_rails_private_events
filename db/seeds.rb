5f96ecbbecb5d575c1454802daf61b3144b65425
Cheong <xxlsxx1955@gmail.com>
2023-03-30 19:36:59 +0800

    Include seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+emails = %w[foo@coldmail.com bar@coldmail.com binz@coldmail.com]
+password = "tuna123"
+locations = ["center town hall", "benny's backyard", "lakeside garden"]
+
+emails.each do |email|
+  User.create email: email, password: password, password_confirmation: password
+end
+
+locations.each_with_index do |location, index|
+  Event.create date: Date.today, location: location, creator_id: index
+end
