cab46dd9a24f0cf6d319419ac1e7085b40e642b6
Cheong <xxlsxx1955@gmail.com>
2023-03-31 10:25:19 +0800

    Establish m:n associations between User and Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :participations, foreign_key: "attended_event_id"
+  has_many :attendees, through: :participations
 
   validates :date, presence: true
   validates :location, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, foreign_key: "creator_id"
+  has_many :participations, foreign_key: "attendee_id"
+  has_many :attended_events, through: :participations
 end
