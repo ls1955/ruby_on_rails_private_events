c61d03d9bd4c6778d120959f5cbdf9d281bfc932
Cheong <xxlsxx1955@gmail.com>
2023-03-31 13:30:40 +0800

    Include events#edit view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,9 +24,17 @@ class EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find params[:id]
   end
 
   def update
+    @event = Event.find params[:id]
+
+    if @event.update event_params
+      redirect_to event_path(@event), notice: "Event has been successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
